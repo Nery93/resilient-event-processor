cc33101d665975bc1b7cce15ac2c942373286008
Nery93 <guilherme.rp93@hotmail.com>
2026-09-22 19:51:58 +0100

    refactor: rename Event struct to EventRequest and remove Timestamp field

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -2,11 +2,9 @@ package domain
 
 import (
 	"encoding/json"
-	"time"
 )
 
-type Event struct {
+type EventRequest struct {
 	EventID   string          `json:"event_id"`
 	Payload   json.RawMessage `json:"payload"`
-	Timestamp time.Time       `json:"timestamp"`
 }
